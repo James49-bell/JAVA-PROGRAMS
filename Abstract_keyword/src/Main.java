707c4298c29e72383eac40b3a0fdbc5386057a07
J JAMES <67492046+James49-bell@users.noreply.github.com>
2021-05-09 17:52:16 +0530

    Add files via upload

---
+++ Abstract_keyword/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		// abstract = abstract classes cannot be instantiated, but they can have
+		// a subclass abstract method are declare without an implementation
+
+		car car = new car();
+		// Vehicle vehicle = new Vehicle();
+		car.go();
+	}
+
+}
---
+++ Abstract_keyword/src/car.java
@@ -0,0 +1,8 @@
+public class car extends Vehicle {
+
+	void go() {
+		System.out.println(" The driver is driving the car");
+
+	}
+
+}
