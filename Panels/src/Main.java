420c1bde0740b31b49f5efe4b8fe68f968fa7970
J JAMES <67492046+James49-bell@users.noreply.github.com>
2021-05-09 18:15:58 +0530

    Add files via upload

---
+++ Panels/src/Main.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// JPanle = a aGUI component that function as a container to hold other
+		// components
+		ImageIcon icon = new ImageIcon("pc6.png");
+		JLabel label = new JLabel();
+
+		label.setText("Hiii");
+		label.setIcon(icon);
+		label.setVerticalAlignment(JLabel.TOP);
+		JPanel redpanel = new JPanel();
+		redpanel.setBackground(Color.red);
+		redpanel.setBounds(0, 0, 250, 250);
+
+		JPanel bluepanel = new JPanel();
+		bluepanel.setBackground(Color.blue);
+		bluepanel.setBounds(250, 0, 250, 250);
+
+		JPanel greenpanel = new JPanel();
+		greenpanel.setBackground(Color.green);
+		greenpanel.setBounds(0, 250, 500, 250);
+
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setSize(750, 750);
+		redpanel.add(label);
+		frame.setVisible(true);
+
+		frame.add(redpanel);
+		frame.add(bluepanel);
+		frame.add(greenpanel);
+	}
+
+}
