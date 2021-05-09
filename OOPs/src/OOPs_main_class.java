01f82b85a6d264049a60e080e4fc047f6c2c898b
J JAMES <67492046+James49-bell@users.noreply.github.com>
2021-05-09 18:15:43 +0530

    Add files via upload

---
+++ OOPs/src/OOPs_main_class.java
@@ -0,0 +1,11 @@
+public class OOPs_main_class {
+
+	public static void main(String[] args) {
+		sub_class mysub_class = new sub_class();
+
+		System.out.println(mysub_class.model);
+		mysub_class.brake();
+
+	}
+
+}
